4b593aa439fdd4f3d0053e95fa3c53ea168726b1
C.K.Sun <fgriasa123@gmail.com>
2018-06-11 13:03:57 +0800

    Update

---
+++ Hard/023_Merge_k_Sorted_Lists.py
@@ -0,0 +1,36 @@
+"""
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+from Queue import PriorityQueue
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        temp = ListNode(None)
+        curr = temp
+        q = PriorityQueue()
+        for node in lists:
+            if node: q.put((node.val,node))
+        while q.qsize()>0:
+            curr.next = q.get()[1]
+            curr=curr.next
+            if curr.next: q.put((curr.next.val, curr.next))
+        return temp.next
